a497c8a3d1db9324b9a446583b5ffb2bb8d361ba
Erick141ds <Demiscarreon00@gmail.com>
2024-10-18 11:24:23 -0600

    ejercicio14.py

--- ejercicio14.py
+++ ejercicio14.py
@@ -1,16 +1,11 @@
 '''
 Dado un número de dos cifras, diseñe un algoritmo que permita obtener el 
 número invertido.
-entadas:
-dijito1:
-dijito2:
+entradas:
+n1:
 salidas:
-numeroin:
+ni
 '''
-ni = int(input("escibe el numero: "))
-d1 = int(input("escribe el dijito1: "))
-d2 = int(input("escribe el dijito2: "))
-d1 = ( ni / 10 )
-d2 = ( ni % 10 )
-iv = ( d2 * 10 + d1 )
-print("el inveriso de n1 es: ",iv)
+n1 = int(input("escibe el numero: "))
+ni = int(str(n1)[::-1])
+print("el inveriso de n1 es: ",ni)
