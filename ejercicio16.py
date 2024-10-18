b61165e50ef069f3030a69b7c16f3aa2d9b80535
Erick141ds <Demiscarreon00@gmail.com>
2024-10-18 13:26:05 -0600

    ejercicio3.py

---
+++ ejercicio16.py
@@ -0,0 +1,13 @@
+'''
+Dos vehículos viajan a diferentes velocidades (v1 y v2) y están distanciados por 
+una distancia d. 
+entradas:
+v1
+v2
+salidas:
+distancia:
+'''
+v1 = int(input("escribe la velocidad del auto1: "))
+v2 = int(input("escribe la velocidad del auto2: "))
+d = ( v2 - v1 )
+print("la distancia de los autos es: ",d)
