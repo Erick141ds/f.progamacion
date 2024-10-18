26b4014df56d968d34824356e6a2c38029e90ddd
Erick141ds <Demiscarreon00@gmail.com>
2024-10-18 11:14:00 -0600

    ejercicio13.py

--- ejercicio13.py
+++ ejercicio13.py
@@ -8,7 +8,7 @@ raiz cuadrada
 raiz cubica
 '''
 v1 = int(input("escribe el valor 1: "))
-rc = ( v1 / 2 )
-rcu = ( v1 / 3 ) 
+rc = ( v1 ** (1/2) )
+rcu = ( v1 ** (1/3) )
 print("la raíz cuadrada del valor1 es:",rc)
 print("la raíz cúbica del valor1 es:",rcu)
